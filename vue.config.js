b26fd7c7aa13460a12b5034c018ac351825b911f
uulala <18059451066@163.com>
2021-07-13 10:52:34 +0800

    fix: 优化配置

--- vue.config.js
+++ vue.config.js
@@ -1,48 +1,46 @@
 const path = require('path');
 
-console.log('23:',path.resolve(__dirname, './build/md-loader/index.js'))
-
+// console.log(process.env)
 module.exports = {
     configureWebpack: config => {
-        console.log(config.mode)
-        if (process.env.NODE_ENV === 'production') {
-            // 为生产环境修改配置...
+        console.log('mode-----:', config.mode)
+        // if (process.env.NODE_ENV === 'production') {
+        //     // 为生产环境修改配置...
+        // } else {
+        //     // 为开发环境修改配置...
+        // }
 
-        } else {
-            // 为开发环境修改配置...
-            return {
-                // entry: './src/laUi/',
-
-                resolve: {
-                    // 路径别名
-                    alias: {
-                        '@': path.resolve('src'),
-                        main: path.resolve(__dirname, 'src/laUi'),
-                        packages: path.resolve(__dirname, 'src/laUi/packages'),
-                        examples: path.resolve(__dirname, 'src/laUi/examples'),
-                    },
+        return {
+            // entry: './src/laUi/',
+            resolve: {
+                // 路径别名
+                alias: {
+                    '@': path.resolve('src'),
+                    main: path.resolve(__dirname, 'src/laUi'),
+                    packages: path.resolve(__dirname, 'src/laUi/packages'),
+                    examples: path.resolve(__dirname, 'src/laUi/examples'),
                 },
-                module: {
-                    rules: [
-                        {
-                            test: /\.md$/,
-                            use: [
-                                {
-                                    loader: 'vue-loader',
-                                    options: {
-                                        compilerOptions: {
-                                            preserveWhitespace: false
-                                        }
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.md$/,
+                        use: [
+                            {
+                                loader: 'vue-loader',
+                                options: {
+                                    compilerOptions: {
+                                        preserveWhitespace: false
                                     }
-                                },
-                                {
-                                    loader: path.resolve(__dirname, './build/md-loader/index.js')
                                 }
-                            ]
-                        },
-                    ]
-                }
+                            },
+                            {
+                                loader: path.resolve(__dirname, './build/md-loader/index.js')
+                            }
+                        ]
+                    },
+                ]
             }
         }
-    },
+    }
 }
